765f4bc7c9a4b72d23a4fd0b521a825a324c4bdd
cleonavila <cleon@almagrario.com>
2026-04-05 21:39:01 -0500

    Ajustes

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
+5. Reconstruir data
+```
+http://localhost:3000/api/v2/seed
+```
 
 ## Stack usado
 * Mongo DB
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -8,24 +8,36 @@ import { PokemonAPIResponse } from './interfaces/pokemon-api.response';
 export class SeedService {
 
   constructor(
-      @InjectModel(Pokemon.name)
-      private readonly pokemonModel: Model<Pokemon>
-    ) {
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {
 
   }
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=600`);
+
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=650`);
     const json: PokemonAPIResponse = await response.json();
-    const { results = [] } = json; 
-    const bulkCreate:any[] = [];
+    const { results = [] } = json;
+    const bulkCreate: {name:string, no: number}[]= [];
+    /*
+    #Insersión de registros mediante promesas
+    const bulkCreate: any[] = [];
     results.forEach(({name, url}) => {
       const segmentos = url.split('/');
       const no:number = +segmentos[segmentos.length - 2];
       bulkCreate.push(this.pokemonModel.create({no, name}));
     });
-    await Promise.all(bulkCreate);
+    await Promise.all(bulkCreate);*/
+    results.forEach(({ name, url }) => {
+      const segmentos = url.split('/');
+      const no: number = +segmentos[segmentos.length - 2];
+      bulkCreate.push({no, name});
+    });
+    await this.pokemonModel.insertMany(bulkCreate);
     return 'Seed executed';
   }
+
 }
